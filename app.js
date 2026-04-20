8acba0ca231c4d903261be76b915ffc832cdb3a0
DEVESH TIWARI <deveshups@gmail.com>
2026-04-21 01:33:22 +0530

    4,5 question

--- app.js
+++ app.js
@@ -5,3 +5,18 @@
 let b = true; //true = 1 only in js false = 0
 b++; // 1+1
 console.log(b); // 2
+
+
+// Question 4
+
+let a=13++
+console.log(a)  //error uninary op ko sirf variable pe laga sakte hai constant pe nahi
+
+
+// Question 5
+
+let x = 10
+let y = ++(x++)
+console.log(y);  //eroor theek upar jaise aya 
+
+//  y = ++10  // error uninary on constant isliye
