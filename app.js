b16f556e20b05e8a5e48b99064c6e42507c80616
DEVESH TIWARI <deveshups@gmail.com>
2026-04-22 15:31:12 +0530

    question 8 , 9

--- app.js
+++ app.js
@@ -1,22 +1,52 @@
+// let totalPrice = Number(prompt("Enter Total Ammount : "));
+// console.log(totalPrice);
 
+// if (Number.isNaN(totalPrice)) {
+//   console.log("Enter Valid Price...");
+// } else if (0 < totalPrice && totalPrice <= 5000) {
+//   let finalPrice = totalPrice;
+//   console.log(
+//     `You Got 0% discount then Your Current Bill Price is ${finalPrice}  `,
+//   );
+// } else if (5001 < totalPrice && totalPrice <= 7000) {
+//   let discount = Math.floor((totalPrice * 5) / 100);
+//   let finalPrice = totalPrice - discount;
+//   console.log(
+//     `You Got 5% discount then Your Current Bill Price is ${finalPrice}  `,
+//   );
+// } else if (7001 < totalPrice && totalPrice <= 9000) {
+//   let discount = Math.floor((totalPrice * 10) / 100);
+//   let finalPrice = totalPrice - discount;
+//   console.log(
+//     `You Got 10% discount then Your Current Bill Price is ${finalPrice}  `,
+//   );
+// } else if (totalPrice > 9001) {
+//   let discount = Math.floor((totalPrice * 15) / 100);
+//   let finalPrice = totalPrice - discount;
+//   console.log(
+//     `You Got 15% discount then Your Current Bill Price is ${finalPrice}  `,
+//   );
+// }
 
-// Question 3
+let totalPrice = Number(prompt("Enter Total Ammount : "));
+console.log(totalPrice);
+let discount = 0;
 
-let b = true; //true = 1 only in js false = 0
-b++; // 1+1
-console.log(b); // 2
+if (Number.isNaN(totalPrice)) {
+  console.log("Enter Valid Price...");
+} else if (0 < totalPrice && totalPrice <= 5000) {
+  discount = 0;
+} else if (5001 < totalPrice && totalPrice <= 7000) {
+  discount = 5;
+} else if (7001 < totalPrice && totalPrice <= 9000) {
+  discount = 10;
+} else if (totalPrice > 9001) {
+  discount = 20;
+}
 
+console.log(
+  ` You Got ${discount}% discount the total price is   ${totalPrice - Math.trunc((totalPrice * discount) / 100)}`,
+);
 
-// Question 4
-
-let a=13++
-console.log(a)  //error uninary op ko sirf variable pe laga sakte hai constant pe nahi
-
-
-// Question 5
-
-let x = 10
-let y = ++(x++)
-console.log(y);  //eroor theek upar jaise aya 
-
-//  y = ++10  // error uninary on constant isliye
+// let otp = Math.trunc(Math.random() * 90000 + 10000);
+// console.log(otp);
