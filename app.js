1580a98691ddcce24967cd636bcff173f235370f
DEVESH TIWARI <deveshups@gmail.com>
2026-04-25 02:20:52 +0530

    guess the number game

--- app.js
+++ app.js
@@ -1,31 +1,28 @@
-let pr = prompt("Enter Number  : ");
+let computerNum = Math.floor(Math.random() * 100) + 1;
 
-if (pr === null) {
-  console.log("Cancelled");
-} else {
-  n = Number(pr);
-  if (isNaN(n)) {
-    console.log("Enter Valid Number...");
+let userNum;
+
+while (true) {
+  let input = prompt("Guess the Number (1-100):");
+
+  if (input === null) {
+    console.log("Game Cancelled");
+    break;
+  }
+
+  userNum = Number(input);
+
+  if (isNaN(userNum) || userNum < 1 || userNum > 100) {
+    console.log("Enter valid number between 1 to 100");
+    continue;
+  }
+
+  if (userNum > computerNum) {
+    console.log("Too High");
+  } else if (userNum < computerNum) {
+    console.log("Too Low");
   } else {
-    if (n > 0) {
-      let sum = 0;
-      let copy = n;
-      while (n > 0) {
-        let rem = n % 10;
-        let fact = 1;
-        for (let i = 1; i <= rem; i++) {
-          fact = fact * i;
-        }
-        sum = sum + fact;
-        n = Math.floor(n / 10);
-      }
-      if (copy === sum) {
-        console.log("Given Number --> Strong Number");
-      } else {
-        console.log("Given Number --> Not Strong Number");
-      }
-    } else {
-      console.log("should be + and more than 0");
-    }
+    console.log("Congratulations... ❤️");
+    break;
   }
 }
