614627cbcfb72ae7a91e001eb01f1d81f0d15641
DEVESH TIWARI <deveshups@gmail.com>
2026-04-25 22:24:46 +0530

    pattern programming ques

--- app.js
+++ app.js
@@ -1,28 +1,10 @@
-let computerNum = Math.floor(Math.random() * 100) + 1;
+let prompt = require("prompt-sync")();
 
-let userNum;
+let n = prompt("enter the number ");
 
-while (true) {
-  let input = prompt("Guess the Number (1-100):");
-
-  if (input === null) {
-    console.log("Game Cancelled");
-    break;
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= n; j++) {
+    process.stdout.write("* ");
   }
-
-  userNum = Number(input);
-
-  if (isNaN(userNum) || userNum < 1 || userNum > 100) {
-    console.log("Enter valid number between 1 to 100");
-    continue;
-  }
-
-  if (userNum > computerNum) {
-    console.log("Too High");
-  } else if (userNum < computerNum) {
-    console.log("Too Low");
-  } else {
-    console.log("Congratulations... ❤️");
-    break;
-  }
+  console.log();
 }
