84764fdb6989bdfd11a7e99638c5223ba3471f54
DEVESH TIWARI <deveshups@gmail.com>
2026-04-26 15:03:04 +0530

    pattern programming ques

--- app.js
+++ app.js
@@ -2,9 +2,22 @@ let prompt = require("prompt-sync")();
 
 let n = prompt("enter the number ");
 
+// for (let i = 1; i <= n; i++) {
+//   let ascii = 65;
+//   for (let j = 1; j <= i; j++) {
+//     process.stdout.write(String.fromCharCode(ascii) + " ");
+//     ascii++;
+//   }
+//   console.log();
+// }
 for (let i = 1; i <= n; i++) {
-  for (let j = 1; j <= n; j++) {
-    process.stdout.write("* ");
+  //spaces
+  for (let j = 1; j <= n - i; j++) {
+    process.stdout.write(" ");
+  }
+  //stars
+  for (let k = 1; k <= i; k++) {
+    process.stdout.write("*");
   }
   console.log();
 }
